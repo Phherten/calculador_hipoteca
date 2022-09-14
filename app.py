64b98fc093b508b14a2fb1265fbd112308776aee
Phherten <98804730+Phherten@users.noreply.github.com>
2022-09-14 16:13:27 +0200

    Acabado, falta subirlo a heroku

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request
 from main import hipoteca
+import requests
+import json
 
 app = Flask(__name__)
 
 
-# http://localhost:3001/
 @app.route('/', methods=['POST'])
 def inicio():
-    cuota = hipoteca(int(215000), float(2.1), int(25))
-    return f'La cuota a pagar es {str(cuota)}'
+    request_body = request.get_json()
+    bot_token = "5435944228:AAH9T6qKkSAO-zZjEIYkMn1WuHUSQtuxOcM"
+    bot_chat_id = str(request_body['bot_chadid'])
+    cuota = hipoteca(request_body['total'], request_body['interes_ofrecido'], request_body['años'])
+    msg = f'Es Lunes, pero alegrate porque pagarias {cuota} euros de hipoteca si la hubieras cogido variable'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chat_id + '&parse_mode=Markdown&text=' + msg
+    response = requests.get(send_text)
+    return "mensaje enviado"
--- main.py
+++ main.py
@@ -2,18 +2,31 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import bs4
+import requests
+from bs4 import BeautifulSoup
 
 
-def hipoteca(total, tipo, años):
+def hipoteca(total, tipo_ofrecido, años):
     total = total
-    tipo = tipo
+    euribor = euribor_scraping()
+    tipo = tipo_ofrecido + euribor
     interes_mensual = (tipo / 12) / 100
     años = años
-    cuotas = 25 * 12
+    cuotas = años * 12
     cuota_mensual = total * (
-                (((1 + interes_mensual) ** cuotas) * interes_mensual) / (((1 + interes_mensual) ** cuotas) - 1))
+            (((1 + interes_mensual) ** cuotas) * interes_mensual) / (((1 + interes_mensual) ** cuotas) - 1))
 
     return round(cuota_mensual)
 
 
+def euribor_scraping():
+    resultado = requests.get('https://www.expansion.com/mercados/euribor.html')
+    sopa = bs4.BeautifulSoup(resultado.text, 'lxml')
+    valor = sopa.select('tbody tr td')[0]
+    euribor = valor.getText()
+    decimal = euribor.replace(',', '.')
+    return float(decimal)
 
+
+print(hipoteca(215000,1,25))
